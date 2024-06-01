4ca4553a820a2266e35181cee0aee25f309797b3
thinnerthinker <thinnrthinkr@gmail.com>
2024-06-01 10:42:17 +0300

    enable autoUpgrade

--- configuration.nix
+++ configuration.nix
@@ -75,6 +75,9 @@ in
 
   hardware.nvidia = import ./src/nvidia.nix config;
 
+  system.autoUpgrade.enable = true;
+  system.autoUpgrade.allowReboot = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
