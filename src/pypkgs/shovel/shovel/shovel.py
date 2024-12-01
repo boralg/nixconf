6701b372f4f493edc865372320647c565daa91fc
thinnerthinker <thinnrthinkr@gmail.com>
2024-12-01 20:18:11 +0200

    do not shovel packages anymore

--- src/pypkgs/shovel/shovel/shovel.py
+++ src/pypkgs/shovel/shovel/shovel.py
@@ -8,35 +8,6 @@ import uuid
 
 NIXCONF_HOME = os.environ.get("NIXCONF_HOME", "/home/yallo/nixconf")
 
-def get_pkgs():
-    with open(NIXCONF_HOME + "/src/pkgs.nix", 'r') as file:
-        lines = file.readlines()
-
-    return lines[0], lines[2:-1]
-
-def set_pkgs(header, packages):
-    content = [header, '['] + packages + [']']
-
-    with open(f"{NIXCONF_HOME}/src/pkgs.nix", 'w') as file:
-        file.write('\n'.join(content))
-
-def add_packages_to_nix(packages):
-    header, existing_packages = get_pkgs()
-
-    # Add the new packages, skipping the ones that are already there
-    for package in packages:
-        if f'  {package}' not in existing_packages:
-            existing_packages.append(f'  {package}')
-
-    set_pkgs(header, existing_packages)
-
-def remove_packages_from_nix(packages):
-    header, existing_packages = get_pkgs()
-
-    # Remove the specified packages
-    existing_packages = [package for package in existing_packages if f'  {package}' not in packages]
-
-    set_pkgs(header, existing_packages)
 
 def sync_nix_conf():
     backup_uuid = str(uuid.uuid4())
@@ -74,21 +45,26 @@ def preprocess_args(args):
     return new_args
 
 def main():
-    args = preprocess_args(sys.argv[1:])
     parser = argparse.ArgumentParser(prog='shovel', 
         description='Shovel, a script to manage a NixOS configuration structured in Shovel-style')
-    parser.add_argument('-i', '--install', nargs='+', help='Add the specified packages to pkgs.nix')
-    parser.add_argument('-u', '--uninstall', nargs='+', help='Remove the specified packages from pkgs.nix')
-    parser.add_argument('-s', '--sync', action='store_true', 
-                        help='Sync the NIXCONF_HOME directory with /etc/nixos and rebuild the system configuration')
-    parser.add_argument('-r', '--rollback', help='Rollback the system configuration to a specified backup')
-    args = parser.parse_args(args)
 
-    if args.install:
-        add_packages_to_nix(args.install)
-    if args.uninstall:
-        remove_packages_from_nix(args.uninstall)
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+        '-s', '--sync',
+        action='store_true',
+        help='Sync the NIXCONF_HOME directory with /etc/nixos and rebuild the system configuration'
+    )
+    group.add_argument(
+        '-r', '--rollback',
+        metavar='BACKUP',
+        help='Rollback the system configuration to a specified backup and rebuild the system configuration'
+    )
+
+    args = parser.parse_args()
+
     if args.sync:
         sync_nix_conf()
-    if args.rollback:
+    elif args.rollback:
         rollback_nix_conf(args.rollback)
+    else:
+        parser.print_help()
