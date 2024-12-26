3bf64005102b56a8d489bd3ec8f68857cac3084b
thinnerthinker <thinnrthinkr@gmail.com>
2024-12-26 23:39:26 +0200

    hyprland: fix login loop

--- modules/desktops/hyprland.nix
+++ modules/desktops/hyprland.nix
@@ -14,6 +14,7 @@ in
 
   config = lib.mkIf cfg.enable {
     programs.hyprland.enable = true;
+    services.displayManager.sddm.enable = true;
 
     environment.systemPackages = [
       pkgs.kitty
