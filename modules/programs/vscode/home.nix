00007fff0606838503b0870b876e907c2b64fb55
boralg <thinnrthinkr@gmail.com>
2025-05-24 09:54:04 +0300

    vscode: update

--- modules/programs/vscode/home.nix
+++ modules/programs/vscode/home.nix
@@ -5,23 +5,25 @@
 
     package = pkgs.unstable.vscodium;
 
-    enableUpdateCheck = false;
-    enableExtensionUpdateCheck = false;
+    profiles.default = {
+      enableUpdateCheck = false;
+      enableExtensionUpdateCheck = false;
 
-    userSettings = builtins.fromJSON (builtins.readFile ./settings.json) // {
-      "rust-analyzer.server.path" = "${pkgs.rust-analyzer}/bin/rust-analyzer";
+      userSettings = builtins.fromJSON (builtins.readFile ./settings.json) // {
+        "rust-analyzer.server.path" = "${pkgs.rust-analyzer}/bin/rust-analyzer";
+      };
+
+      extensions = with pkgs.vscode-extensions.vscode-marketplace; [
+        jnoortheen.nix-ide
+        ms-python.python
+        (ms-python.vscode-pylance.override { meta.license = [ ]; })
+        rust-lang.rust-analyzer
+        wgsl-analyzer.wgsl-analyzer
+        wholroyd.jinja
+
+        mkhl.direnv
+        editorconfig.editorconfig
+      ];
     };
-
-    extensions = with pkgs.vscode-extensions.vscode-marketplace; [
-      jnoortheen.nix-ide
-      ms-python.python
-      (ms-python.vscode-pylance.override { meta.license = [ ]; })
-      rust-lang.rust-analyzer
-      wgsl-analyzer.wgsl-analyzer
-      wholroyd.jinja
-
-      mkhl.direnv
-      editorconfig.editorconfig
-    ];
   };
 }
