142c6ad3e387eb83477696bc65e2e5973d3bd6a6
thinnerthinker <thinnrthinkr@gmail.com>
2025-11-28 21:58:03 +0200

    mouse: proper scroll direction

--- modules/desktops/plasma/home/thinkpad-mouse.nix
+++ modules/desktops/plasma/home/thinkpad-mouse.nix
@@ -1,12 +1,13 @@
 {
   programs.plasma.configFile.kcminputrc = {
     "Libinput/1739/52934/SYNA801A:00 06CB:CEC6 Mouse" = {
-      NaturalScroll = false;
+      NaturalScroll = true;
       ScrollFactor = 0.75;
     };
     "Libinput/2/10/TPPS\\/2 Elan TrackPoint" = {
       PointerAccelerationProfile = 2;
     };
+    "Libinput/1739/52934/SYNA801A:00 06CB:CEC6 Touchpad".NaturalScroll = true;
     Mouse.X11LibInputXAccelProfileFlat = true;
     Mouse.XLbInptPointerAcceleration = "-0.2";
   };
