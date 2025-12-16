7f0dc1baa63303594d1c180845cde63737d3d395
boralg <thinnrthinkr@gmail.com>
2025-12-16 21:30:19 +0200

    install: luks

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 WIFI_SSID=""
 WIFI_PWD=""
+DO_CLONE=1
+DO_ENCRYPT=0
 FORMAT_PART="/dev/nvme0n1"
-SWAP_END="16GB"
 HOSTNAME="dark"
 
 set -e
@@ -12,39 +13,45 @@ if [ -n "$WIFI_SSID" ]; then
     ssid=\"$WIFI_SSID\"
     psk=\"$WIFI_PWD\"
 }" | sudo tee /etc/wpa_supplicant.conf
-
     sudo ip link set wlp0s20f3 up
     sudo wpa_supplicant -B -i wlp0s20f3 -c /etc/wpa_supplicant.conf
     sudo dhcpcd wlp0s20f3
 fi
 
 
-git clone https://github.com/boralg/nixconf.git
+if [ "$DO_CLONE" = "1" ]; then
+    git clone https://github.com/boralg/nixconf.git
+fi
 
 
 sudo parted $FORMAT_PART -- mklabel gpt
 sudo parted $FORMAT_PART -- mkpart ESP fat32 1MB 512MB
 sudo parted $FORMAT_PART -- set 1 esp on
-sudo parted $FORMAT_PART -- mkpart primary linux-swap 512MB $SWAP_END
-sudo parted $FORMAT_PART -- mkpart primary $SWAP_END 100%
+sudo parted $FORMAT_PART -- mkpart primary 512MB 100%
 
 sudo mkfs.fat -F 32 -n boot ${FORMAT_PART}p1
-sudo mkswap -L swap ${FORMAT_PART}p2
-sudo mkfs.btrfs -L nixos ${FORMAT_PART}p3
 
-sudo mount ${FORMAT_PART}p3 /mnt
+if [ "$DO_ENCRYPT" = "1" ]; then
+    sudo cryptsetup --verify-passphrase -v luksFormat ${FORMAT_PART}p2
+    sudo cryptsetup open ${FORMAT_PART}p2 cryptroot
+    ROOT_DEV="/dev/mapper/cryptroot"
+else
+    ROOT_DEV="${FORMAT_PART}p2"
+fi
+
+sudo mkfs.btrfs -L nixos $ROOT_DEV
+
+sudo mount $ROOT_DEV /mnt
 sudo btrfs subvolume create /mnt/root
 sudo btrfs subvolume create /mnt/home
 sudo btrfs subvolume create /mnt/nix
 sudo umount /mnt
 
-sudo mount -o subvol=root,compress=zstd,noatime ${FORMAT_PART}p3 /mnt
+sudo mount -o subvol=root,compress=zstd,noatime $ROOT_DEV /mnt
 sudo mkdir -p /mnt/{home,nix,boot}
-sudo mount -o subvol=home,compress=zstd,noatime ${FORMAT_PART}p3 /mnt/home
-sudo mount -o subvol=nix,compress=zstd,noatime ${FORMAT_PART}p3 /mnt/nix
+sudo mount -o subvol=home,compress=zstd,noatime $ROOT_DEV /mnt/home
+sudo mount -o subvol=nix,compress=zstd,noatime $ROOT_DEV /mnt/nix
 sudo mount ${FORMAT_PART}p1 /mnt/boot
-sudo swapon ${FORMAT_PART}p2
-
 
 sudo mkdir -p /mnt/home/bora
 sudo cp -r nixconf /mnt/home/bora/
@@ -59,4 +66,13 @@ mkdir -p ~/.config/nix
 echo "experimental-features = nix-command flakes" > ~/.config/nix/nix.conf
 
 
-echo "sudo nixos-install --flake .#$HOSTNAME"
+if [ "$DO_ENCRYPT" = "1" ]; then
+    echo ""
+    echo "IMPORTANT: Add this to hardware-configuration.nix:"
+    echo 'boot.initrd.luks.devices."cryptroot".device = "/dev/disk/by-uuid/'$(sudo blkid -s UUID -o value ${FORMAT_PART}p2)'";'
+    echo ""
+fi
+
+
+cd /mnt/home/bora/nixconf
+echo "Run: sudo nixos-install --flake .#$HOSTNAME"
